bbcb61c2ffae95a5bacb035ae7f21023bc1ad14c
HeleneGuibaud <helene.guibaud@hotmail.com>
2020-04-23 22:44:27 +0200

    Ajout de l'exercice 5 - Partie 2.

---
+++ Partie 2 - Les conditions/Exercice_5/Assets/JS/script.js
@@ -0,0 +1,25 @@
+let day = 7;
+
+switch (day) {
+    case 1:
+        alert (`Lundi`);
+    break;
+    case 2:
+        alert (`Mardi`);
+    break;
+    case 3:
+        alert (`Mercredi`);
+    break;
+    case 4:
+        alert (`Jeudi`);
+    break;
+    case 5:
+        alert (`Vendredi`);
+    break;
+    case 6:
+        alert (`Samedi`);
+    break;
+    case 7:
+        alert (`Dimanche`);
+    break;
+}
