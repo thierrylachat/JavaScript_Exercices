fa29094fc5dbfd8b4823e55e8a9c9ec5c5f3dc65
HeleneGuibaud <helene.guibaud@hotmail.com>
2020-04-25 22:53:01 +0200

    Partie 3 - Exercices 1 à 8 réalisés.

--- Partie 3 - Les fonctions javascript/partie-03/src/exercicesAnswers.js
+++ Partie 3 - Les fonctions javascript/partie-03/src/exercicesAnswers.js
@@ -1,46 +1,46 @@
 //Exercice 1 : Donner la taille de la chaîne de caractères.
 let stringLength = function (sentence) {
-  return sentence.length ;
+  return sentence.length;
 }
 
 //Exercice 2 : Remplacer le premier "e" de la chaîne par un espace.
 let replaceFirstEBySpace = function (sentence) {
-  return 'A compléter';
+  return sentence.replace("debugger","d bugger");
 }
 
 //Exercice 3 : Concaténer les deux chaînes de caractères.
 let concatenateSentences = function (firstSentence, secondSentence) {
-  return 'A compléter';
+  return firstSentence.concat(secondSentence);
 }
 
 //Exercice 4 : Afficher le cinquième caractère de la chaîne.
 let displayFifthCharacter = function (sentence) {
-  return 'A compléter';
+  return sentence.slice(4,5);
 }
 
 //Exercice 5 : Afficher les 9 premiers caractère.
 let displayFirstNineCharacters = function (sentence) {
-  return 'A compléter';
+  return sentence.slice(0,9);
 }
 
 //Exercice 6 : Mettre en majuscule la chaîne.
 let upperTheSentence = function (sentence) {
-  return 'A compléter';
+  return sentence.toUpperCase();
 }
 
 //Exercice 7 : Mettre en minuscule la chaîne.
 let lowerTheSentence = function (sentence) {
-  return 'A compléter';
+  return sentence.toLowerCase();
 }
 
 //Exercice 8 : Supprimer les espaces avant et après la chaîne.
 let removeSpaces = function (sentence) {
-  return 'A compléter';
+  return sentence.trim();
 }
 
 //Exercice 9 : Afficher true si le paramètre d'entrée de la fonction est de type *string*.
 let isString = function (sentence) {
-  return 'A compléter';
+  return 'A compléter'
 }
 
 //Exercice 10 : Afficher l'extension du fichier.
@@ -60,7 +60,7 @@ let invertSentence = function (sentence) {
 
 //Exercice 13 : Chercher si dans la chaîne de caractère se trouve "La Manu".
 let searchInSentence = function (sentence) {
-  return 'A compléter';
+  return sentence.indexOf("La Manu");
 }
 
 //Exercice 14 : Afficher la valeur absolue d'un nombre.
