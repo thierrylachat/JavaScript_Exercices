2d0fe03ac977886e15ca5b42e53af97acdef2f32
HeleneGuibaud <helene.guibaud@hotmail.com>
2020-04-27 10:37:12 +0200

    Partie 4 - Exercices 1, 2 et 3 réalisés.

---
+++ Partie 4 - Les tableaux et les fonctions/src/exercicesAnswers.js
@@ -0,0 +1,70 @@
+//Exercice 1 : Créer un tableau days et lui ajouter les jours de la semaine.
+let createArray = function () {
+  return createArray = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+}
+
+//Exercice 2 : Retouner le 3ème élément du tableau days', function() {
+let returnThirdElement = function (array) {
+  return array[2];
+}
+
+//Exercice 3 : Afficher la valeur de l\'index 3 du tableau days.', function() {
+let returnThirdIndex = function (array) {
+  return array[3];
+}
+
+//Exercice 4 : Changer le 2ème élément du tableau languages par php.', function() {
+let changeSecondElement = function (array) {
+  return array;
+  array[1] = "php";
+  }
+
+//Exercice 5 : Ajouter ruby et python à la fin du tableau languages.', function() {
+let addAtTheEnd = function (array) {
+  return 'A compléter';
+}
+
+//Exercice 6 : Ajouter html et css au début du tableau languages.', function() {
+let addAtFirst = function (array) {
+  return 'A compléter';
+}
+
+//Exercice 7 : Supprimer le premier élément du tableau languages.', function() {
+let removeFirstElement = function (array) {
+  return 'A compléter';
+}
+
+//Exercice 8 : Supprimer le dernier élément du tableau languages.', function() {
+let removeLastElement = function (array) {
+  return 'A compléter';
+}
+
+//Exercice 9 : Créer un tableau associatif person. Lui ajouter les index name, age et city avec les valeurs Jean, 45, Paris.', function() {
+let createPerson = function () {
+  return 'A compléter';
+}
+
+//Exercice 10 : Retourner la valeur de l\'index nom du tableau person.', function() {
+let returnName = function (array) {
+  return 'A compléter';
+}
+
+//Exercice 11 : Stocker tous les index du tableau person dans des variables séparées et les retourner dans une même phrase de type : "Bonjour, je suis Jean. J\'ai 45 ans et j\'habite Paris."', function() {
+let returnSentence = function (array) {
+  return 'A compléter';
+}
+
+//Exercice 12 : Transformer la variable string en tableau.', function() {
+let returnArray = function (string) {
+  return 'A compléter';
+}
+
+//Exercice 13 : Trier le tableau array.', function() {
+let sortArray = function (array) {
+  return 'A compléter';
+}
+
+//Exercice 14 : Transformer le tableau array en chaîne de caractères', function() {
+let returnString = function (array) {
+  return 'A compléter';
+}
