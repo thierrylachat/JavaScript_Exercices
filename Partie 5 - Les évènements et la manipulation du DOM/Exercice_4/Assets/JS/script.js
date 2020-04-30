8f80d16f001d724eccef5c449884c2307128fdd8
HeleneGuibaud <helene.guibaud@hotmail.com>
2020-04-30 11:11:10 +0200

    Partie 5 - Exercice 4 non OK.

--- Partie 5 - Les évènements et la manipulation du DOM/Exercice_4/Assets/JS/script.js
+++ Partie 5 - Les évènements et la manipulation du DOM/Exercice_4/Assets/JS/script.js
@@ -1,16 +1,15 @@
+// Sources des images :
 // https://www.w3schools.com/js/pic_bulboff.gif
 // https://www.w3schools.com/js/pic_bulbon.gif
 
-// Sélection de la balise img via son sélecteur css
+
+// Sélection de la balise img via son sélecteur css.
 let img = document.querySelector('img');
 
-// Rattachement d'un évènement à l'élément html img
-img.addEventListener('click', function() {
-  this.src = 
+
+// Rattachement d'un évènement à l'élément html img.
+img.addEventListener('click', function(){
+  //This fait référence à l'objet courant, en l'occurence l'img qui a été cliquée.
+  this.src = "https:www.w3schools.com/js/pic_bulbon.gif";
+  this.setAttribute(alt, Ampoule éteinte));
 }
-
-
-
-
-
-
