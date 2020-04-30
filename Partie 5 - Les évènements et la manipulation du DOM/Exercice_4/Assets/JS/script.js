e99e9d6965b8287fd671b2ada6c6682cb49c2d16
HeleneGuibaud <helene.guibaud@hotmail.com>
2020-04-30 14:09:39 +0200

    Partie 5 - Exercice 4 finalisé.

--- Partie 5 - Les évènements et la manipulation du DOM/Exercice_4/Assets/JS/script.js
+++ Partie 5 - Les évènements et la manipulation du DOM/Exercice_4/Assets/JS/script.js
@@ -11,5 +11,11 @@ let img = document.querySelector('img');
 img.addEventListener('click', function(){
   //This fait référence à l'objet courant, en l'occurence l'img qui a été cliquée.
   this.src = "https:www.w3schools.com/js/pic_bulbon.gif";
-  this.setAttribute(alt, Ampoule éteinte));
-}
+  this.setAttribute('alt', 'Ampoule allumée');
+});
+
+// Changement de la taille de l'image au survol de la souris.
+img.addEventListener('mouseover', function(){
+  this.style.width = "300px";
+  this.style.height = "540px";
+});
