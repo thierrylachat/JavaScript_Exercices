4690827bde4be897b6f4eace9943b706648406a8
HeleneGuibaud <helene.guibaud@hotmail.com>
2020-05-01 22:42:23 +0200

    Partie 5 - Exercice 5 non OK.

--- Partie 5 - Les évènements et la manipulation du DOM/Exercice_5/Assets/JS/script.js
+++ Partie 5 - Les évènements et la manipulation du DOM/Exercice_5/Assets/JS/script.js
@@ -1,33 +1,30 @@
-// function myFunction() {
-//   document.getElementsById("python").style.color = "pink";
-// }
-
-
-
-//Solution 1.
-
-//
-function changeColor(){
+// Détermination de la fonction qui permet de changer la couleur du paragraphe.
+function changeColor() {
+  // Récupération de l'id de l'élément déclencheur de la fonction.
   let color = this.id;
   document.querySelector('p').style.color = color;
 }
-function changeFont(){
-  let changeType = this.id;
-  if(changeType == 'bold){
-    document.querySelector('p').style.fontWeight = changeType;
+
+// Détermination de la fonction qui permet de changer la police de texte.
+function changeFont() {
+  // Récupération de l'id de l'élément déclencheur de la fonction.
+  let font = this.id;
+  // On teste le type de modification à faire sur le texte.
+  if(font == 'bold'){
+    document.querySelector('p').style.fontWeight = font;
   } else {
-    document.querySelector ('p').style.fontWeight = changeType;
+    document.querySelector('p').style.fontStyle = font;
   }
 }
 
-//Renvoie un table contenant les boutons ayant l'id color.
-let buttonsList = document.querySelectorAll('.color');
-buttonsList.forEach(function(button){
+// Renvoi d'un table contenant les boutons ayant l'id color.
+let buttonsColorList = document.querySelector('.color');
+buttonsColorList.forEach(function(button){
   button.addEventListener('click', changeColor);
-})
-//Renvoie un table contenant les boutons ayant l'id font.
-let buttonsList = document.querySelectorAll('.color');
-buttonsList.forEach(function(button){
-  button.addEventListener('click', changeColor);
-})
+});
 
+//Renvoi d'un table contenant les boutons ayant l'id font.
+let buttonsStyleList = document.querySelector('.font');
+buttonsStyleList.forEach(function(button){
+  button.addEventListener('click', changeFont);
+});
---
+++ Partie 6 - Les boucles/Exercice_1/Assets/JS/script.js
@@ -0,0 +1,15 @@
+// let i = 2020;
+// let text = "L'année";
+
+// while (i <= 2030) {
+//   text += text +i;
+//   i++;
+// }
+
+// document.getElementById("years").innerHTML = text;
+
+for(year=2020; year<=2030; year++){
+  let tr = document.createElement('tr');
+  let td = document.createElement('td');
+
+}
