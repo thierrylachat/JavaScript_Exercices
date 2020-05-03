1ace71d90c3e7ec0504143dea15d7096a63761c5
HeleneGuibaud <helene.guibaud@hotmail.com>
2020-05-03 14:50:55 +0200

    Partie 6 - Exercice 1 finalisé.

--- Partie 6 - Les boucles/Exercice_1/Assets/JS/script.js
+++ Partie 6 - Les boucles/Exercice_1/Assets/JS/script.js
@@ -1,15 +1,27 @@
-// let i = 2020;
-// let text = "L'année";
+let tbody = document.querySelector('tbody');
 
-// while (i <= 2030) {
-//   text += text +i;
-//   i++;
-// }
+// Création de la boucle.
+for (year = 2020; year <= 2030; year++) {
 
-// document.getElementById("years").innerHTML = text;
-
-for(year=2020; year<=2030; year++){
+  // Création de la ligne.
   let tr = document.createElement('tr');
+
+  // Création de la cellule.
   let td = document.createElement('td');
+
+  // Insertion de la date dans la cellule.
+  td.textContent = year;
+
+  // Recherche et affichage des années bissextiles.
+  if ((year % 400 == 0) || ((year % 4 == 0) && !(year % 100 == 0))) {
+    td.style.color = 'white';
+    td.style.backgroundColor = 'IndianRed';
+  }
+
+  // Insertion de la cellule dans une ligne.
+  tr.appendChild(td);
+
+  // Insertion de la ligne dans le tableau.
+  tbody.appendChild(tr);
 
 }
