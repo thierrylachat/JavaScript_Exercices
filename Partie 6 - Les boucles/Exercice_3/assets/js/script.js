fed4b7486fcbd4666367372400f8668b15e1769c
HeleneGuibaud <helene.guibaud@hotmail.com>
2020-05-03 20:52:28 +0200

    Partie 6 - Exercice 3.

---
+++ Partie 6 - Les boucles/Exercice_3/assets/js/script.js
@@ -0,0 +1,40 @@
+// SOLUTION 1 OK
+let languages = ['html', 'css', 'javascript', 'php', 'mysql', 'c++','ruby','python'];
+
+let x;
+
+for (x of languages) {
+    document.write(x + ' ');
+}
+
+
+
+// // SOLUTION 2 NOK
+// let languages = ['html', 'css', 'javascript', 'php', 'mysql', 'c++','ruby','python'];
+// let p = document.createElement('p');
+// let body = document.createElement('body');
+// let i = 0;
+// let text = '';
+
+// for (;languages[i];) {
+//     text += languages[i] + ' ';
+//     i++;
+//     p.textContent = text;
+//     body.appendChild(p);
+// }
+
+
+
+//SOLUTION 3 NOK
+// let languages = ['html', 'css', 'javascript', 'php', 'mysql', 'c++', 'ruby', 'python'];
+// let p = document.createElement('p');
+// let body = document.createElement('body');
+// let i = 0;
+// let text = '';
+
+// while (languages[i]) {
+//     text += languages[i] + ' ';
+//     i++;
+//     p.textContent = text;
+//     body.appendChild(p);
+// }
