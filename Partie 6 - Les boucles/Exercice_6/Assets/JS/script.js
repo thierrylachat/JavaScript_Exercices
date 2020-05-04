76ee9bab725a8c9e5b31ad5441ce1f1496846495
HeleneGuibaud <helene.guibaud@hotmail.com>
2020-05-04 21:02:59 +0200

    Partie 7.

--- Partie 6 - Les boucles/Exercice_6/Assets/JS/script.js
+++ Partie 6 - Les boucles/Exercice_6/Assets/JS/script.js
@@ -1,8 +1,20 @@
-let firstNumber = Math.floor(Math.random()*10) + 1;
-let secondNumber = Math.floor(Math.random()*10) + 1;
+// let firstNumber = Math.floor(Math.random()*10) + 1;
+// let secondNumber = Math.floor(Math.random()*10) + 1;
 
-while (firstNumber / secondNumber > 1) {
-  //secondNumber = secondNumber * firstNumber;
+// while (firstNumber / secondNumber > 1) {
+//   firstNumber /= secondNumber;
+//   console.log(firstNumber);
+// }
+
+// SOLUTION 2.
+
+let firstNumber = Math.floor(Math.random()*10 + 1);
+let secondNumber = Math.floor(Math.random()*10 + 1);
+
+do {
   firstNumber /= secondNumber;
+  if(firstNumber < 1){
+    break;
+  }
   console.log(firstNumber);
-}
+} while(true);
