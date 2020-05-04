10911792603427bc4a0981302e606fae2529238d
HeleneGuibaud <helene.guibaud@hotmail.com>
2020-05-04 21:24:49 +0200

    Partie 6 - Exercice 4 finalisé.

--- Partie 6 - Les boucles/Exercice_4/assets/js/script.js
+++ Partie 6 - Les boucles/Exercice_4/assets/js/script.js
@@ -1,45 +1,13 @@
 let days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi','Dimanche'];
 
-
-// SOLUTION 1 NOK.
-// let text = '';
-
-// for (i = 0; i <= 6;) {
-//     text += days[i] + ' ';
-//     i++;
-    
-//     if ((i > 4) && (i <= 6)) {
-//         let span = document.createElement('span');
-//         span.textContent = text;
-//         span.style.fontWeight = 'bold';
-//     }
-
-//     document.getElementById("week").innerHTML = text;
-
-// }
-
-
-// SOLUTION 2 NOK.
-
-// days.forEach(function(item, index, array) {
-//     document.write(item + ' ');
-// });
-
-// let saturday = days[5];
-// let sunday = days[6];
-
-// document.querySelector('saturday').style.fontWeight = 'bold';
-// sunday.style.fontWeight = 'bold';
-
-
-// SOLUTION 3.
-
 let div = document.createElement('div');
 
-languages.forEach(function(day) {
+// Création d'une boucle forEach() pour parcourir le tableau.
+days.forEach(function(day) {
     let p = document.createElement('p');
     p.textContent = day;
-    if(day.toLowerCase() === 'samedi' || day.toLowerCase === 'dimanche') {
+    // Conversion du texte en majuscule ou en minuscule afin de comparer des valeurs comparables.
+    if(day.toLowerCase() === 'samedi' || day.toLowerCase() === 'dimanche') {
         p.style.fontWeight = 'bold';
     }
     div.appendChild(p);
