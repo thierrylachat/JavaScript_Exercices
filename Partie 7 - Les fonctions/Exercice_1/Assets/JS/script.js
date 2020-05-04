00f7b0c3b54ab3b553b893a2fe4ef086d86a86a3
HeleneGuibaud <helene.guibaud@hotmail.com>
2020-05-04 23:29:08 +0200

    Partie 7 - Exercice 1 non OK.

--- Partie 7 - Les fonctions/Exercice_1/Assets/JS/script.js
+++ Partie 7 - Les fonctions/Exercice_1/Assets/JS/script.js
@@ -1,5 +1,13 @@
+let number1 = document.getElementById('input_1');
+let number2 = document.getElementById('input_2');
+let product = number1 * number2;
+
 function multiply(number1, number2) {
   return number1 * number2;
-}
+};
 
-console.log(multiply(2,2));
+document.getElementsByTagName('button').addEventListener('click', multiply {
+  document.getElementById('product');
+});
+
+
