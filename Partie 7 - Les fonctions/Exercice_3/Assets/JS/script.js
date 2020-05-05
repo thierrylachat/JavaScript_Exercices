54d86ea1c4f848c7d293a0dc3f819f102c346f21
HeleneGuibaud <helene.guibaud@hotmail.com>
2020-05-05 21:51:02 +0200

    Partie 7 - Exercice 3 finalisé.

--- Partie 7 - Les fonctions/Exercice_3/Assets/JS/script.js
+++ Partie 7 - Les fonctions/Exercice_3/Assets/JS/script.js
@@ -1,18 +1,3 @@
-function multiply(firstNumber, secondNumber) {
-    return firstNumber * secondNumber;
-}
-
-document.querySelector('#result').addEventListener('click', function() {
-    let firstNumber = document.querySelector('#input_1').value;
-    let secondNumber = document.querySelector('#input_2').value;
-    document.querySelector('#label').textContent = multiply(firstNumber, secondNumber);
-});
-
-
-/* 
-
-Solution 2 avec fonction fléchée.
-
 let multi = (firstNumber, secondNumber) => firstNumber * secondNumber;
 
 document.querySelector('#result').addEventListener('click', function () {
@@ -20,5 +5,3 @@ document.querySelector('#result').addEventListener('click', function () {
     let secondNumber = document.querySelector('#input_2').value;
     document.querySelector('#label').textContent = multi(firstNumber, secondNumber);
 });
-
-*/
