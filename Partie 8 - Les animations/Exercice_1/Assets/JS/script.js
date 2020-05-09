aabd10d8b659d1d7eeb3e7a246b074e12a0b3632
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-05-09 22:03:10 +0200

    Partie 8 - Exercice 1 finalisé.

--- Partie 8 - Les animations/Exercice_1/Assets/JS/script.js
+++ Partie 8 - Les animations/Exercice_1/Assets/JS/script.js
@@ -1,13 +1,9 @@
-// ===== Scroll to Top ==== 
-$(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(200);    // Fade in the arrow
-    } else {
-        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
-    }
-});
-$('#return-to-top').click(function() {      // When arrow is clicked
-    $('body,html').animate({
-        scrollTop : 0                       // Scroll to top of body
-    }, 500);
+let btnScrollToTop = document.querySelector('#btnScrollToTop');
+
+btnScrollToTop.addEventListener('click', function () {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    });
 });
