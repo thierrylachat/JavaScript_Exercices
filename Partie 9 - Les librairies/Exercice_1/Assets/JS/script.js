b1a77f18bf80bc37773ed38ccce4b100e2b56e9a
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-05-11 15:32:48 +0200

    Partie 9 - Exercice 1

---
+++ Partie 9 - Les librairies/Exercice_1/Assets/JS/script.js
@@ -0,0 +1,31 @@
+/* 
+Sources : https://www.youtube.com/watch?v=qNMPcfEBmNI&feature=youtu.be
+*/
+
+var cleave = new Cleave('#date', {
+    date: true,
+    delimiter: '-',
+    datePattern: ['d', 'm', 'Y']
+});
+
+var cleave = new Cleave('#creditCardNumber', {
+    creditCard: true,
+    onCreditCardTypeChanged: function (type) {
+        
+        if (type === 'visa') {
+            document.querySelector('.fa-cc-visa').classList.add('active');
+        } else {
+            document.querySelector('.fa-cc-visa').classList.remove('active');
+        }
+        if (type === 'mastercard') {
+            document.querySelector('.fa-cc-mastercard').classList.add('active');
+        } else {
+            document.querySelector('.fa-cc-mastercard').classList.remove('active');
+        }
+        if (type === 'amex') {
+            document.querySelector('.fa-cc-amex').classList.add('active');
+        } else {
+            document.querySelector('.fa-cc-amex').classList.remove('active');
+        }
+    }
+});
