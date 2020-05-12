4a97fd58d105733e381041e62d1c8c2e94cb43f8
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-05-12 11:59:59 +0200

    Partie 9 - Exercice 2 finalisé.

--- Partie 9 - Les librairies/Exercice_2/Assets/JS/script.js
+++ Partie 9 - Les librairies/Exercice_2/Assets/JS/script.js
@@ -1,1 +1,6 @@
-/* Sources : https://ireade.github.io/inlinetweetjs/ */
+/* 
+Sources : 
+https://ireade.github.io/inlinetweetjs/
+https://github.com/ireade/inlinetweetjs
+https://codepen.io/isaacpante/details/qaxoJL
+ */
